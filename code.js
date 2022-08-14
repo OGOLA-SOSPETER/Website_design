384ed7d767dc018a3eaf34415acf6b8b380e9658
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-08-14 23:06:04 +0300

    web development

---
+++ code.js
@@ -0,0 +1,33 @@
+///*
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", function() {
+    alert("Welcome.\This is Captain Sos");
+})
+btn.addEventListener("click", function() {
+    var age = prompt("Input my age:");
+    var f = age;
+    alert(age);
+    // var st = "Your age= " + f;
+    //alert = console.log(st);
+    //"Your age =", +f)
+})
+btn.onclick(alert("CONTINUE"));
+btn.onload(alert("Welcome.."));
+//*/
+
+const btn1 = document.getElementById("btn1");
+
+btn1.addEventListener("click", function() {
+    alert("Welcome.\This is Captain Sos");
+})
+btn1.addEventListener("click", function() {
+    var nam = prompt("Input your name:");
+    var n = nam;
+    alert(n);
+    var stn = "Your name= ";
+    alert = console.log(stn);
+    //"Your age =", +f)
+})
+btn1.onclick(alert("CONTINUE"));
+btn1.onload(alert("Welcome.."));
